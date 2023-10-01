2998498f2bd838f8c3f4c3de9adbd2feb5d88f82
Himanshu Raj <himanshurajkush@gmail.com>
2023-10-01 15:31:36 +0530

    AndroidProducts: add COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 # Calling our device product
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X00T-eng \
+    lineage_X00T-userdebug \
+    lineage_X00T-user
