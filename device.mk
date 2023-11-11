0a9fa4be3555361a996aa2b8eca855e134e5f989
Himanshu Raj <himanshurajkush@gmail.com>
2023-11-11 23:52:23 +0530

    device: build libdrm from external/libdrm install into /vendor
    lib: libdrm.so
    
    error:
    E vndksupport: Could not load /vendor/lib64/hw/hwcomposer.sdm660.so from default namespace: dlopen failed: library "libdrm.so" not found: needed by /vendor/lib64/libsdmcore.so in namespace (default).

--- device.mk
+++ device.mk
@@ -23,7 +23,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor
+    android.hardware.drm@1.2.vendor \
+    libdrm.vendor
 
 # Gatekeeper Vendor
 PRODUCT_PACKAGES += \
